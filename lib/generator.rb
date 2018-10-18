2826b7747997ccdf2cca140efa8c2d20ef85078b
Valter Martinek <martinek@freevision.sk>
2018-10-18 11:44:04 +0200

    Update ruby, bundle. Fix some generating issues in new version.

--- lib/generator.rb
+++ lib/generator.rb
@@ -7,13 +7,13 @@ class Generator
   def build(story)
     @book = GEPUB::Book.new
 
-    book.set_primary_identifier   story.identifier, 'BookID', 'URL'
+    book.primary_identifier   story.identifier, 'BookID', 'URL'
 
-    book.add_title     story.title
-    book.add_creator   story.author
+    book.title     story.title
+    book.creator   story.author
 
-    book.add_publisher story.publisher, nil
-    book.add_date      story.published_at.to_s, nil
+    book.publisher story.publisher, nil
+    book.date      story.published_at.to_s, nil
 
     book.ordered {
       story.chapters.each do |chapter|
